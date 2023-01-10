c7e63d51eb8441473018d13a529099f5ae658e3f
NoizuHika <47974646+NoizuHika@users.noreply.github.com>
2023-01-10 13:57:56 +0100

    Update ProductController.java

--- API/src/main/java/com/example/Project/controller/ProductController.java
+++ API/src/main/java/com/example/Project/controller/ProductController.java
@@ -26,21 +26,21 @@ public class ProductController {
         if (filter == null) {
             return productRepository.findAll();
         } else {
-            return productRepository.findByNameContaining(filter).stream().limit(10).collect(Collectors.toList());
+            return productRepository.findByNameContaining(filter).stream().limit(50).collect(Collectors.toList());
         }
     }
 
     @GetMapping("/test")
     @ApiOperation(value = "Otrzymanie listy produktów")
     public List<Product> putNewRandomProduct() {
-        var p = new Product(null,"mars",0.1,0.1,Math.random() * 10.0,10.0);
-        var a = new Product(null,"mleko",0.1,0.1,Math.random() * 10.0,10.0);
-        var b = new Product(null,"makrel",0.1,0.1,Math.random() * 10.0,10.0);
-        var c = new Product(null,"marchew",0.1,0.1,Math.random() * 10.0,10.0);
-        var d = new Product(null,"mors",0.1,0.1,Math.random() * 10.0,10.0);
-        var e = new Product(null,"manna",0.1,0.1,Math.random() * 10.0,10.0);
-        var g = new Product(null,"mod",0.1,0.1,Math.random() * 10.0,10.0);
-        var h = new Product(null,"mor",0.1,0.1,Math.random() * 10.0,10.0);
+        var p = new Product(null,"mars",0.1,0.1,10.0,10.0);
+        var a = new Product(null,"mleko",0.1,0.1,10.0,10.0);
+        var b = new Product(null,"makrel",0.1,0.1,10.0,10.0);
+        var c = new Product(null,"marchew",0.1,0.1,10.0,10.0);
+        var d = new Product(null,"mors",0.1,0.1,10.0,10.0);
+        var e = new Product(null,"manna",0.1,0.1,10.0,10.0);
+        var g = new Product(null,"mod",0.1,0.1,10.0,10.0);
+        var h = new Product(null,"mor",0.1,0.1,10.0,10.0);
 
         productRepository.save(p);
         productRepository.save(a);
